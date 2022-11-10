1de081043623fc38b981d1f6112fc377fe8e4add
Uliana Gritchina <ugritchina@icloud.com>
2022-11-10 15:20:45 +0300

    Initial Commit

---
+++ GitHubUsers/GitHubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersApp.swift
+//  GitHubUsers
+//
+//  Created by Ульяна Гритчина on 10.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
