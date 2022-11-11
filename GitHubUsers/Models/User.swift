4f2b341892be3a07604ffa82bda7a81ca412fddf
Uliana Gritchina <ugritchina@icloud.com>
2022-11-11 16:22:24 +0300

    Added User model.

---
+++ GitHubUsers/Models/User.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+
+struct User {
+    let login: String
+    let avatar_url: String
+    let html_url: String
+    let repos_url: String
+    let name: String
+    let location: String
+    let bio: String
+    let public_repos: String
+    let followers: Int
+    let following: Int
+    let created_at: String
+}
+
